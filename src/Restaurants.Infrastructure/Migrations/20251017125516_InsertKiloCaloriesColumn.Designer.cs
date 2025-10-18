d51ddcd1b289de2363ea839db9eaf0d29bda3567
Mazen Elnems <mazenelnems22@gmail.com>
2025-10-18 13:27:06 +0300

    Add KiloCalories column into Dishs Table

--- src/Restaurants.Infrastructure/Migrations/20251017125516_InsertKiloCaloriesColumn.Designer.cs
+++ src/Restaurants.Infrastructure/Migrations/20251017125516_InsertKiloCaloriesColumn.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurants.Infrastructure.Data;
 
@@ -10,9 +11,11 @@ using Restaurants.Infrastructure.Data;
 namespace Restaurants.Infrastructure.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251017125516_InsertKiloCaloriesColumn")]
+    partial class InsertKiloCaloriesColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
