2ac97486ac3cf74c9cd29c7c96c1e0b895f277dc
Mazen Elnems <mazenelnems22@gmail.com>
2025-11-03 16:50:47 +0200

    add application logic testing

---
+++ tests/Restaurants.Application.Tests/Validators/CreateDishCommandValidatorTest.cs
@@ -0,0 +1,59 @@
+﻿using AutoFixture;
+using Restaurants.Application.Commands.Dishes.CreateDish;
+using FluentValidation.TestHelper;
+
+namespace Restaurants.Application.Tests.Validators;
+
+public class CreateDishCommandValidatorTest 
+{
+    private CreateDishCommandValidator validator;
+
+    public CreateDishCommandValidatorTest()
+    {
+        validator = new CreateDishCommandValidator();
+    }
+
+    [Fact]
+    public void Validator_ValidCreateDishCommand_ShouldNotHaveAnyValidationErrors()
+    {
+        // Arrange
+        var command = new CreateDishCommand
+        {
+            Name = "Test Name",
+            Description = "Test Description",
+            IsAvailable = true,
+            KiloCalories = 1,
+            CategoryId = 1,
+            Price = 1.0m,
+            RestaurantId = 1,
+        };
+
+        // Act
+        var result = validator.TestValidate(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Validator_InValidCreateDishCommand_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+        var command = new CreateDishCommand
+        {
+            Name = new string('a',201),
+            CategoryId = 0,
+            Price = 0,
+            KiloCalories = -1,
+        };
+
+        // Act
+        var result = validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+        result.ShouldHaveValidationErrorFor(x => x.CategoryId);
+        result.ShouldHaveValidationErrorFor(x => x.Price);
+        result.ShouldHaveValidationErrorFor(x => x.KiloCalories);
+    }
+}
